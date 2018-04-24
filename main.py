3a1b30faed2867576ac0b910c505f8699abdfebb
muthuubalakan <muthuumuthu@gmail.com>
2018-04-24 10:29:42 +0200

    Fix server_error

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 #
 # Simple aiohttp login system with default user!.
-# This system is built with aiohttp module.
-# Requires Python3 or higher.
+# This system has been built by using aiohttp module.
+# Require Python3 or higher.
 
 
 import os
@@ -21,18 +21,14 @@ COOKIE_VALUE = 0
 HOST = '127.0.0.1'
 PORT = 8080
 
-
-# All html and other files required from outside.
-# All these files required by the server, modifying contents inside the file
-# may cause server error.
+# Html and json files defined!
 LOGIN_HTML = 'login.html'
 SITE_HTML = 'site.html'
 REDIRECT_HTML = 'redirect.html'
 CONFIG_FILE = 'configuration.json'
 
 
-# Simple error web view for server error.
-# Server error can be caused by missing required files.
+# Server error html for web view
 ERROR = '''<!DOCTYPE html>
 <html>
   <head>
@@ -53,43 +49,45 @@ ERROR = '''<!DOCTYPE html>
 </html>'''
 
 
-# Check the requested file by user, and return server error if the file is
-# missing.
-class FileReader:
+# Login handles checking, reading files and user credentials
+# We use cookie on client site after user successfully logged in
+class Login:
+
+    # Check html and json files
     def _check_the_file(self, filename):
         self.__filename = filename
         _file = os.path.isfile(self.__filename)
         if not _file:
-            print("Internal server error! file not found:{}".format(self.__filename))
+            print("Internal server error! " \
+                  "file not found:{}".format(self.__filename))
             return False
+
+        # Read html files
         if self.__filename.endswith(".html"):
             response = open(self.__filename).read()
             return response
+
+        # Read json file
         elif self.__filename.endswith(".json"):
             configure_file = open(self.__filename)
             load_data = json.load(configure_file)
-            try:
-                if not(load_data['USERNAME'] and load_data['PASSWORD']):
-                    return False
+            if all(key in load_data for key in("USERNAME", "PASSWORD")):
                 return load_data
-            except KeyError:
-                print("{} doesn't have valid keys".format(self.__filename))
-                return False
+            return False
+
+        # Unsupported file format
         else:
-            message = ("{} is not in required format".format(self.__filename))
+            message = ("{} is not in a required format".format(self.__filename))
             print(message)
             return False
 
-
-class Login(FileReader):
-    def __init__(self):
-        super().__init__()
-
     def _check_cookie(self, request):
+        # Get cookie value
         cookie_value = request.cookies.get(COOKIE_NAME, None)
         if not cookie_value:
             return False
         try:
+            # check the requested cookie value
             if not int(cookie_value) == COOKIE_VALUE:
                 return False
             return True
@@ -101,20 +99,17 @@ class Login(FileReader):
         credentials = self._check_the_file(CONFIG_FILE)
         if not credentials:
             return False
-        USERNAME = credentials['USERNAME']
-        PASSWORD = credentials['PASSWORD']
-        if not(self.username == USERNAME and self.password == PASSWORD):
+        username = credentials['USERNAME']
+        password = credentials['PASSWORD']
+        if not (self.username == username and self.password == password):
             return False
         return True
 
     async def server_error(self, request):
-        valid_cookie = self._check_cookie(request)
-        if not valid_cookie:
-            check_login_file = self._check_the_file(LOGIN_HTML)
-            if not check_login_file:
-                return web.Response(text=ERROR, content_type='text/html')
-            return web.HTTPFound('/log')
-        return web.HTTPFound('/')
+        """ Error when a requested file
+        or content of the file is missing.
+        """
+        return web.Response(text=ERROR, content_type='text/html')
 
     async def home(self, request):
         site_file = self._check_the_file(SITE_HTML)
@@ -144,21 +139,23 @@ class Login(FileReader):
         return web.HTTPFound('/')
 
     async def login(self, request):
+        if not self._check_the_file(CONFIG_FILE):
+            return web.HTTPFound('/error')
         form = await request.post()
         self.username = form.get('username')
         self.password = form.get('password')
-        if not self._check_the_file(CONFIG_FILE):
-            return web.HTTPFound('/error')
         if not self._check_credentials():
             return web.HTTPFound('/redirect')
+        # Set the time for 30 days long
+        time_ = datetime.datetime.utcnow() + datetime.timedelta(days=30)
+        cookie_expiry_date = time_.strftime("%a, %d %b %Y %H:%M:%S GMT")
         response = web.HTTPFound('/')
-        expires = datetime.datetime.utcnow() + datetime.timedelta(days=30)
-        COOKIE_EXPIRE = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
         response.set_cookie(COOKIE_NAME,
-                            value=COOKIE_VALUE, expires=COOKIE_EXPIRE)
+                            value=COOKIE_VALUE, expires=cookie_expiry_date)
         return response
 
 
+# Add routes
 def aiohttp_login():
     app = web.Application()
     app.router.add_routes([web.get('/log', Login().login_page),
